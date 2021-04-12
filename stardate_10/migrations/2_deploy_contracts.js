bc63dc15019c5bee0da1408e7d17c48f2bae1d9d
witwiki <witwiki@users.noreply.github.com>
2021-04-12 16:11:18 +1200

    migrated and deployed

--- stardate_10/migrations/2_deploy_contracts.js
+++ stardate_10/migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
-// const ConvertLib = artifacts.require("ConvertLib");
-// const MetaCoin = artifacts.require("MetaCoin");
-const LunarEscrow = artifacts.require("LunarEscrow");
+const SuperStrings = artifacts.require("./SuperString.sol");
 
-module.exports = function(deployer, _network, accounts) {
-  deployer.deploy(LunarEscrow, accounts[0], accounts[1], 1000000);
-  // deployer.deploy(ConvertLib);
-  // deployer.link(ConvertLib, MetaCoin);
-  // deployer.deploy(MetaCoin);
+module.exports = function(deployer) {
+  deployer.deploy(SuperStrings);
 };
